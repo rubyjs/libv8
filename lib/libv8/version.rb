32d1924fe2d6ac5a20334b1d14040acc57cf1ee0
Logan Lowell <fractaloop@thefrontside.net>
2011-06-15 15:49:28 -0500

    V8_VERSION extracted from the git submodule.

--- lib/libv8/version.rb
+++ lib/libv8/version.rb
@@ -1,4 +1,5 @@
 module Libv8
-  V8_VERSION = "3.3.10"
-  VERSION = V8_VERSION + ".2"
+  V8_VERSION = Dir.chdir(File.join(File.dirname(__FILE__), 'v8')) { `git tag`.split.sort.last }
+  REVISION = ""
+  VERSION = V8_VERSION + REVISION
 end
