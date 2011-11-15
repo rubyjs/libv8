a8fed38f2faf80f3105b8471f0fc146f064e3d9a
Charles Lowell <cowboyd@thefrontside.net>
2011-11-15 08:50:07 -0600

    .4 point release

--- lib/libv8/version.rb
+++ lib/libv8/version.rb
@@ -1,6 +1,6 @@
 module Libv8
   version_file = File.join(File.dirname(__FILE__), 'VERSION')
   V8_VERSION = File.exist?(version_file) ? File.read(version_file).chomp : "0.0"
-  REVISION = ""
+  REVISION = ".4"
   VERSION = V8_VERSION + REVISION
 end
