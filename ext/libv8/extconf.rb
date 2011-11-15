ec4bc3b60e609d44e60c743b27609c569a617418
Charles Lowell <cowboyd@thefrontside.net>
2011-11-15 09:02:36 -0600

    don't crash when we don't have rubygems there.

--- ext/libv8/extconf.rb
+++ ext/libv8/extconf.rb
@@ -6,13 +6,16 @@ begin
     printf("Unable to build libv8: %s\n", str)
     exit 1
   end
-  
+
   print "Checking for Python..."
   version = `python --version 2>&1`
   crash "Python not found!" if version.split.first != "Python"
+  require 'rubygems'
   crash "Python 3.x is unsupported by V8!" if Gem::Version.new(version.split.last) >= Gem::Version.new(3)
   crash "Python 2.4+ is required by V8!" if Gem::Version.new(version.split.last) < Gem::Version.new("2.4")
   puts version.split.last
+rescue StandardError => e
+  warn e
 end
 
 
