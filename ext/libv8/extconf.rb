993e91bf05900550c6a3431e2872bc0fc3ca7c8f
Geoff Garside <geoff@geoffgarside.co.uk>
2012-05-12 11:39:52 +0100

    Include Libv8::Make in extconf.rb

--- ext/libv8/extconf.rb
+++ ext/libv8/extconf.rb
@@ -1,8 +1,10 @@
 require 'mkmf'
 create_makefile('libv8')
 require File.expand_path '../arch.rb', __FILE__
+require File.expand_path '../make.rb', __FILE__
 
 include Libv8::Arch
+include Libv8::Make
 
 Dir.chdir(File.expand_path '../../../vendor/v8', __FILE__) do
   puts `make #{libv8_arch}.release`
