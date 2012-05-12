0f6dd145e486c2f3843b12f14cca06f671e69e58
Geoff Garside <geoff@geoffgarside.co.uk>
2012-05-12 15:44:21 +0100

    Include Compiler detection into extconf.rb

--- ext/libv8/extconf.rb
+++ ext/libv8/extconf.rb
@@ -2,10 +2,12 @@ require 'mkmf'
 create_makefile('libv8')
 require File.expand_path '../arch.rb', __FILE__
 require File.expand_path '../make.rb', __FILE__
+require File.expand_path '../compiler.rb', __FILE__
 
 include Libv8::Arch
 include Libv8::Make
+include Libv8::Compiler
 
 Dir.chdir(File.expand_path '../../../vendor/v8', __FILE__) do
-  puts `#{make} #{libv8_arch}.release GYPFLAGS="-Dhost_arch=#{libv8_arch}"`
+  puts `env CXX=#{compiler} LINK=#{compiler} #{make} #{libv8_arch}.release GYPFLAGS="-Dhost_arch=#{libv8_arch}"`
 end
