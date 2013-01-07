3019c31d833afd1e1e3f37750a0f2611a6159c9b
ignisf <bordjukov@gmail.com>
2013-01-07 04:26:54 +0200

    Add methods for make flags to Libv8::Builder

--- ext/libv8/builder.rb
+++ ext/libv8/builder.rb
@@ -1,3 +1,4 @@
+require 'mkmf'
 require File.expand_path '../compiler', __FILE__
 require File.expand_path '../arch', __FILE__
 require File.expand_path '../make', __FILE__
@@ -8,14 +9,28 @@ module Libv8
     include Libv8::Compiler
     include Libv8::Make
 
-    def build_libv8!
+    def gyp_flags(*flags)
+      # Fix a compilation failure under MacOS.
+      # See https://groups.google.com/d/topic/v8-users/Oj-efHLjygc/discussion
+      flags << "-Dhost_arch=#{libv8_arch}" if RUBY_PLATFORM.include?("darwin")
+
+      # FreeBSD uses gcc 4.2 by default which leads to
+      # compilation failures due to warnings about aliasing.
+      flags << "-Dv8_no_strict_aliasing=1" if RUBY_PLATFORM.include?("freebsd") and !check_gcc_compiler(compiler)
+
+      %Q(GYPFLAGS+="#{flags.join(' ')}") unless flags.empty?
+    end
+
+    def make_flags(*flags)
       profile = enable_config('debug') ? 'debug' : 'release'
+      flags << gyp_flags
 
-      gypflags = ["-Dhost_arch=#{libv8_arch}"]
-      gypflags << "-Dv8_no_strict_aliasing=1" if RUBY_PLATFORM.include?("freebsd") && !check_gcc_compiler(compiler)
+      "#{libv8_arch}.#{profile} #{flags.join ' '}"
+    end
 
+    def build_libv8!
       Dir.chdir(File.expand_path '../../../vendor/v8', __FILE__) do
-        puts `env CXX=#{compiler} LINK=#{compiler} #{make} #{libv8_arch}.#{profile} GYPFLAGS="#{gypflags.join ' '}"`
+        puts `env CXX=#{compiler} LINK=#{compiler} #{make} #{make_flags}`
       end
       return $?.exitstatus
     end
