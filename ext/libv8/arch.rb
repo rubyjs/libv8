8e6bd00c358e57520b8c94a781147b705a0ce2fc
ignisf <bordjukov@gmail.com>
2014-06-17 13:30:30 +0300

    Add experimental arm64 support

--- ext/libv8/arch.rb
+++ ext/libv8/arch.rb
@@ -6,10 +6,11 @@ module Libv8
     module_function
     def libv8_arch
       case Gem::Platform.local.cpu
-      when /^arm/           then 'arm'
-      when /^mips/          then 'mips'
-      when /^amd64|^x86_64/ then 'x64'
-      when /^i[3456]86/     then 'ia32'
+      when /^armv8|^aarch64/ then 'arm64'
+      when /^arm/            then 'arm'
+      when /^mips/           then 'mips'
+      when /^amd64|^x86_64/  then 'x64'
+      when /^i[3456]86/      then 'ia32'
       else raise "Unsupported target: #{target}"
       end
     end
