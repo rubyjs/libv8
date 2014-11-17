096464b6d312af262895b45229c0eb57ed6adf59
Petko Bordjukov <bordjukov@gmail.com>
2014-11-17 22:53:44 +0200

    Add AArch64 support in the architecture detection
    Tested with Debian Daily image in Qemu

--- ext/libv8/arch.rb
+++ ext/libv8/arch.rb
@@ -6,9 +6,10 @@ module Libv8
     module_function
     def libv8_arch
       case Gem::Platform.local.cpu
-      when /^arm$/     then 'arm'
-      when /^x86$/     then 'ia32'
-      when /^x86_64$/  then 'x64'
+      when /^arm$/          then 'arm'
+      when /^a(rm|arch)64$/ then 'arm64'
+      when /^x86$/          then 'ia32'
+      when /^x86_64$/       then 'x64'
       else
         warn "Unsupported target: #{Gem::Platform.local.cpu}"
         Gem::Platform.local.cpu
