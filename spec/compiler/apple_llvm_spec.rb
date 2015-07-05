f7b2181f0dd24c5222de6ccdefe0dcffb79b20bf
Petko Bordjukov <bordjukov@gmail.com>
2015-07-05 17:17:22 +0300

    Migrate to RSpec 3 and add some more specs

--- spec/compiler/apple_llvm_spec.rb
+++ spec/compiler/apple_llvm_spec.rb
@@ -7,14 +7,14 @@ module Libv8::Compiler
 
     describe '#name' do
       it 'returns Apple LLVM' do
-        subject.name.should eq 'Apple LLVM'
+        expect(subject.name).to eq 'Apple LLVM'
       end
     end
 
     describe '#version' do
       it 'returns the version of the compiler' do
         stub_as_available 'c++', :apple_llvm, '5.1'
-        subject.version.should eq '5.1'
+        expect(subject.version).to eq '5.1'
       end
     end
 
@@ -22,7 +22,7 @@ module Libv8::Compiler
       context 'when Apple LLVM\'s version is >= 4.3' do
         it 'returns true' do
           stub_as_available 'c++', :apple_llvm, '5.1'
-          subject.compatible?.should be_true
+          expect(subject).to be_compatible
         end
       end
     end
--- spec/compiler/gcc_spec.rb
+++ spec/compiler/gcc_spec.rb
@@ -7,14 +7,14 @@ module Libv8::Compiler
 
     describe '#name' do
       it 'returns GCC' do
-        subject.name.should eq 'GCC'
+        expect(subject.name).to eq 'GCC'
       end
     end
 
     describe '#version' do
       it 'returns the version of the compiler' do
         stub_as_available 'c++', :gcc, '4.9.0'
-        subject.version.should eq '4.9.0'
+        expect(subject.version).to eq '4.9.0'
       end
     end
 
@@ -22,14 +22,14 @@ module Libv8::Compiler
       context 'when GCC\'s version is >= 4.3' do
         it 'returns true' do
           stub_as_available 'c++', :gcc, '4.9.0'
-          subject.compatible?.should be_true
+          expect(subject).to be_compatible
         end
       end
 
       context 'when GCC\'s version is < 4.3' do
         it 'returns false' do
           stub_as_available 'c++', :gcc, '4.2.1-freebsd'
-          subject.compatible?.should be_false
+          expect(subject).to_not be_compatible
         end
       end
     end
