4309032240154125c9215337e54825dfc6b81ef1
Marco Mastrodonato <marco.mastrodonato@ergoitalia.it>
2016-01-14 08:49:33 +0100

    Removed all changes

--- ext/libv8/patcher.rb
+++ ext/libv8/patcher.rb
@@ -47,7 +47,7 @@ module Libv8
         applied_patches = f.readlines.map(&:chomp)
 
         (available_patches - applied_patches).each do |patch|
-          `patch -p1 -N < #{patch}#{' --binary' if RUBY_PLATFORM =~ /mingw/}`
+          `patch -p1 -N < #{patch}`
           fail 'failed to apply patch' unless $?.success?
           f.puts patch
         end
