e5524b2f032f3b089ba666851c377d6742fb5e47
Petko Bordjukov <bordjukov@gmail.com>
2016-04-27 19:31:41 +0300

    Remove all conditional logic for the patches
    Not having to silence individual warnings frees us from the burden of
    applying different patches for different compilers and compiler
    versions.

--- ext/libv8/patcher.rb
+++ ext/libv8/patcher.rb
@@ -4,52 +4,15 @@ module Libv8
 
     module_function
 
-    def patch_directories_for(compiler)
-      patch_directories = []
-
-      case
-      when compiler.target =~ /arm/
-        patch_directories << 'arm'
-      end
-
-      case compiler
-      when Compiler::GCC
-        patch_directories << 'gcc48' if compiler.version >= '4.8'
-      when Compiler::Clang
-        patch_directories << 'clang'
-        patch_directories << 'clang33' if compiler.version >= '3.3'
-        patch_directories << 'clang51' if compiler.version >= '5.1'
-        patch_directories << 'clang70' if compiler.version >= '7.0'
-        patch_directories << 'clang73' if compiler.version >= '7.3'
-      end
-
-      patch_directories
-    end
-
-    def patch_directories(*additional_directories)
-      absolute_paths = [PATCH_DIRECTORY]
-
-      additional_directories.each do |directory|
-        absolute_paths << File.join(PATCH_DIRECTORY, directory)
-      end
-
-      absolute_paths.uniq
-    end
-
-    def patches(*additional_directories)
-      patch_directories(*additional_directories).map do |directory|
-        Dir.glob(File.join directory, '*.patch')
-      end.flatten.sort
-    end
-
-    def patch!(*additional_directories)
+    def patch!
       File.open(".applied_patches", File::RDWR|File::CREAT) do |f|
-        available_patches = patches *additional_directories
+        available_patches = Dir.glob(File.join(PATCH_DIRECTORY, '*.patch'))
         applied_patches = f.readlines.map(&:chomp)
 
         (available_patches - applied_patches).each do |patch|
+          puts "Applying #{patch}"
           `patch -p1 -N < #{patch}`
-          fail 'failed to apply patch' unless $?.success?
+          fail "failed to apply patch #{patch}" unless $?.success?
           f.puts patch
         end
       end
