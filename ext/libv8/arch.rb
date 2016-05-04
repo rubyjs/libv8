3da17485950e93dc29e635e6a30bf068b9701b99
Petko Bordjukov <bordjukov@gmail.com>
2016-05-04 16:13:03 +0300

    Match `universal` cpu to x64 to account for OS X

--- ext/libv8/arch.rb
+++ ext/libv8/arch.rb
@@ -10,6 +10,7 @@ module Libv8
       when /^a(rm|arch)64$/   then 'arm64'
       when /^x86$/            then 'ia32'
       when /^(x86_64|amd64)$/ then 'x64'
+      when /^universal$/      then 'x64' # OS X
       else
         warn "Unsupported target: #{Gem::Platform.local.cpu}"
         Gem::Platform.local.cpu
