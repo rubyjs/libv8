10ebc0d5f08fb2ca3f385258c511788e8dda3b23
Petko Bordjukov <bordjukov@gmail.com>
2017-03-29 10:22:13 +0300

    Add armv6 to architecture detection
    Closes #235
    Closes #234

--- ext/libv8/arch.rb
+++ ext/libv8/arch.rb
@@ -6,11 +6,11 @@ module Libv8
 
     def libv8_arch
       case Gem::Platform.local.cpu
-      when /^arm(v7.*)*$/     then 'arm'
-      when /^a(rm|arch)64$/   then 'arm64'
-      when /^x86$/            then 'ia32'
-      when /^(x86_64|amd64)$/ then 'x64'
-      when /^universal$/      then 'x64' # OS X
+      when /^arm(v6.*|v7.*)*$/ then 'arm'
+      when /^a(rm|arch)64$/    then 'arm64'
+      when /^x86$/             then 'ia32'
+      when /^(x86_64|amd64)$/  then 'x64'
+      when /^universal$/       then 'x64' # OS X
       else
         warn "Unsupported target: #{Gem::Platform.local.cpu}"
         Gem::Platform.local.cpu
